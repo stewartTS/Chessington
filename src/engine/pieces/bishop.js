3155a29e09cc93bc6392ef8b41fc305b691b891c
Stewart Bowie <Stewart.Bowie@softwire.com>
2021-06-01 15:38:02 +0100

    Bishop functionality added

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,3 +1,5 @@
+import Player from '../player';
+import Square from '../square';
 import Piece from './piece';
 
 export default class Bishop extends Piece {
@@ -6,6 +8,33 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let locationRow = location.row
+        let locationCol = location.col
+        let locationRow2 = location.row
+        let locationCol2 = location.col
+
+        let moves = [];
+        for (let i = 0; i < 8; i++) {     
+
+            locationRow += 1
+            locationRow2 -= 1
+            locationCol += 1
+            locationCol2 -= 1
+
+            if (locationRow < 8 && locationCol < 8) {
+                moves.push(Square.at(locationRow, locationCol));  
+            }
+            if (locationRow < 8 && locationCol2 > -1) {
+                moves.push(Square.at(locationRow, locationCol2));  
+            }
+            if (locationRow2 > -1 && locationCol2 > -1) {
+                moves.push(Square.at(locationRow2, locationCol2));
+            }
+            if (locationRow2 > -1 && locationCol < 8) {
+                moves.push(Square.at(locationRow2, locationCol));
+            }
+        }
+        return moves;
     }
 }
