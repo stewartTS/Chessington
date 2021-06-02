8cc96f80d7deb87d0fcced82122ab8d8733332ce
Stewart Bowie <Stewart.Bowie@softwire.com>
2021-06-02 12:01:52 +0100

    Pawns can't move if there's another piece

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -10,17 +10,44 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         const moves = [];
+        const square1W = Square.at(location.row + 1, location.col);
+        const square2W = Square.at(location.row + 2, location.col);
+
+        const square1B = Square.at(location.row - 1, location.col);
+        const square2B = Square.at(location.row - 2, location.col);
+        
+
         if (this.player === Player.WHITE) {
-            moves.push(Square.at(location.row + 1, location.col));
+
+            if(board.getPiece(square1W)) {
+                return moves
+            }
+
+            moves.push(square1W);
+
             if (location.row === 1) {
-                moves.push(Square.at(location.row + 2, location.col));
+                if(board.getPiece(square2W)) {
+                    return moves
+                }
+                console.log(board.getPiece(square2W))
+                moves.push(square2W)
             }
         } else {
-            moves.push(Square.at(location.row - 1, location.col));
-            if (location.row === 6) {
-                moves.push(Square.at(location.row - 2, location.col));
+            if(board.getPiece(square1B)) {
+                return moves
             }
+            moves.push(square1B);
+
+            if (location.row === 6) {
+                if(board.getPiece(square2B)) {
+                    return moves
+                }
+                moves.push(square2B)
+            }
+            }
+            return moves;
         }
-        return moves;
+        
     }
-}
+
+
