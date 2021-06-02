a04015f0ad038172473a057452d346fd2938129f
Stewart Bowie <Stewart.Bowie@softwire.com>
2021-06-02 15:35:52 +0100

    Queen blocking functionality added

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -13,18 +13,6 @@ export default class Bishop extends Piece {
         
         const location = board.findPiece(this);
         const moves = [];
-        // for (let i = 1; i < 8; i++) {
-        //     const locationRow = location.row + i
-        //     const locationCol = location.col + i
-        //     const locationRow2 = location.row - i
-        //     const locationCol2 = location.col - i
-
-        //     moves.push(Square.at(locationRow, locationCol));
-        //     moves.push(Square.at(locationRow, locationCol2));
-        //     moves.push(Square.at(locationRow2, locationCol2));
-        //     moves.push(Square.at(locationRow2, locationCol));
-
-        // }
 
         for (let i = 1; this.checkIfOnBoard(location.row + i, location.col + i); i++) {
             if (board.getPiece(Square.at(location.row + i, location.col + i))) {
--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -8,36 +8,64 @@ export default class Queen extends Piece {
 
     getAvailableMoves(board) {
         const location = board.findPiece(this);
-        const allMoves = [];
-        
-        // for (let i = 0; i < 8; i++) {
-        //     if (i !== location.col) {
-        //         allMoves.push(Square.at(location.row, i));
-        //     }
-        //     if (i !== location.row) {
-        //         allMoves.push(Square.at(i, location.col));
-        //     }
-        // }
+        const moves = [];
 
-        for (let i = 1; i < 8; i++) {
-            const locationRowPlus = location.row + i
-            const locationRowMinus = location.row - i
-            const locationColPlus = location.col + i
-            const locationColMinus = location.col - i
-
-            allMoves.push(Square.at(locationRowPlus, locationColPlus));
-            allMoves.push(Square.at(locationRowPlus, locationColMinus));
-            allMoves.push(Square.at(locationRowMinus, locationColMinus));
-            allMoves.push(Square.at(locationRowMinus, locationColPlus));
-            
-            allMoves.push(Square.at(locationRowPlus, location.col));
-            allMoves.push(Square.at(locationRowMinus, location.col));
-            allMoves.push(Square.at(location.row, locationColPlus));
-            allMoves.push(Square.at(location.row, locationColMinus));
-
-
+        for (let i = 1; this.checkIfOnBoard(location.row + i, location.col + i); i++) {
+            if (board.getPiece(Square.at(location.row + i, location.col + i))) {
+                break;
+            }
+            moves.push(Square.at(location.row + i, location.col + i));
         }
-        const legalMoves = allMoves.filter(move => move.row > -1 && move.row < 8 && move.col > -1 && move.col < 8)
-        return legalMoves;
+
+        for (let i = 1; this.checkIfOnBoard(location.row + i, location.col - i); i++) {
+            if (board.getPiece(Square.at(location.row + i, location.col - i))) {
+                break;
+            }
+            moves.push(Square.at(location.row + i, location.col - i));
+        }
+
+        for (let i = 1; this.checkIfOnBoard(location.row - i, location.col - i); i++) {
+            if (board.getPiece(Square.at(location.row - i, location.col - i))) {
+                break;
+            }
+            moves.push(Square.at(location.row - i, location.col - i));
+        }
+
+        for (let i = 1; this.checkIfOnBoard(location.row - i, location.col + i); i++) {
+            if (board.getPiece(Square.at(location.row - i, location.col + i))) {
+                break;
+            }
+            moves.push(Square.at(location.row - i, location.col + i));
+        }
+
+        for (let i = 1; this.checkIfOnBoard(location.row, location.col + i); i++) {
+            if (board.getPiece(Square.at(location.row, location.col + i))) {
+                break;
+            }
+            moves.push(Square.at(location.row, location.col + i));
+        }
+
+        for (let i = 1; this.checkIfOnBoard(location.row + i, location.col); i++) {
+            if (board.getPiece(Square.at(location.row + i, location.col))) {
+                break;
+            }
+            moves.push(Square.at(location.row + i, location.col));
+        }
+        
+        for (let i = 1; this.checkIfOnBoard(location.row, location.col - i); i++) {
+            if (board.getPiece(Square.at(location.row, location.col - i))) {
+                break;
+            }
+            moves.push(Square.at(location.row, location.col - i));
+        }
+
+        for (let i = 1; this.checkIfOnBoard(location.row - i, location.col); i++) {
+            if (board.getPiece(Square.at(location.row - i, location.col))) {
+                break;
+            }
+            moves.push(Square.at(location.row - i, location.col));
+        }
+        return moves;
     }
 }
+
