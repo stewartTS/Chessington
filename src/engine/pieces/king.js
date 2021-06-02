9e579a61b53192f1cf51b696b7c78d6cff4f7f2f
Stewart Bowie <Stewart.Bowie@softwire.com>
2021-06-02 10:50:53 +0100

    King moves added

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,11 +1,35 @@
+import Square from '../square';
 import Piece from './piece';
 
-export default class King extends Piece {
+export default class Queen extends Piece {
     constructor(player) {
         super(player);
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this);
+        const allMoves = [];
+        const legalMoves =[];
+
+        let locationRow = location.row;
+        let locationCol = location.col;
+        
+        allMoves.push([locationRow + 1, locationCol + 1]);
+        allMoves.push([locationRow + 1, locationCol - 1]);
+        allMoves.push([locationRow - 1, locationCol + 1]);
+        allMoves.push([locationRow - 1, locationCol - 1]);
+        allMoves.push([locationRow, locationCol + 1]);
+        allMoves.push([locationRow, locationCol - 1]);
+        allMoves.push([locationRow + 1, locationCol]);
+        allMoves.push([locationRow - 1, locationCol]);
+
+    allMoves.forEach(move => {
+        if(move[0] > -1 && move[0] < 8 && move[1] > -1 && move[1] < 8) {
+            legalMoves.push(Square.at(move[0], move[1]));
+        }
+    });
+
+    return legalMoves;
     }
+
 }
