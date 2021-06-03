0b388c628820dfa7a3d436b8030ba8cf0d1eec75
Stewart Bowie <Stewart.Bowie@softwire.com>
2021-06-03 14:05:25 +0100

    All pieces working, can take pieces

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,6 +1,7 @@
 import Player from '../player';
 import Square from '../square';
 import Piece from './piece';
+import King from './king';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -12,9 +13,13 @@ export default class Pawn extends Piece {
         const moves = [];
         const square1W = Square.at(location.row + 1, location.col);
         const square2W = Square.at(location.row + 2, location.col);
+        const wTakeL = Square.at(location.row + 1, location.col - 1);
+        const wTakeR = Square.at(location.row + 1, location.col + 1);
 
         const square1B = Square.at(location.row - 1, location.col);
         const square2B = Square.at(location.row - 2, location.col);
+        const bTakeL = Square.at(location.row - 1, location.col - 1);
+        const bTakeR = Square.at(location.row - 1, location.col + 1);
 
 
         if (this.player === Player.WHITE) {
@@ -25,6 +30,20 @@ export default class Pawn extends Piece {
 
             if (board.getPiece(square1W)) {
                 return moves
+            }
+
+            console.log(square1W);
+
+            if (this.checkIfOnBoard(wTakeL) && board.getPiece(wTakeL)) {
+                if (board.getPiece(wTakeL).player === Player.BLACK && !(board.getPiece(wTakeL) instanceof King)) {
+                    moves.push(wTakeL);
+                }
+            }
+
+            if (this.checkIfOnBoard(wTakeR) && board.getPiece(wTakeR)) {
+                if (board.getPiece(wTakeR).player === Player.BLACK && !(board.getPiece(wTakeR) instanceof King)) {
+                    moves.push(wTakeR);
+                }
             }
 
             moves.push(square1W);
@@ -45,6 +64,18 @@ export default class Pawn extends Piece {
                 return moves
             }
             moves.push(square1B);
+
+            if (this.checkIfOnBoard(bTakeL) && board.getPiece(bTakeL)) {
+                if (board.getPiece(bTakeL).player === Player.WHITE && !(board.getPiece(bTakeL) instanceof King)) {
+                    moves.push(bTakeL);
+                }
+            }
+
+            if (this.checkIfOnBoard(bTakeR) && board.getPiece(bTakeR)) {
+                if (board.getPiece(bTakeR).player === Player.WHITE && !(board.getPiece(bTakeR) instanceof King))  {
+                    moves.push(bTakeR);
+                }
+            }
 
             if (location.row === 6) {
                 if (board.getPiece(square2B)) {
