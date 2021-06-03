982b94a758343081725468b708c013e915da038a
Stewart Bowie <Stewart.Bowie@softwire.com>
2021-06-03 12:27:22 +0100

    All pieces working apart from pawn

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -8,28 +8,11 @@ export default class Queen extends Piece {
 
     getAvailableMoves(board) {
         const location = board.findPiece(this);
-        const allMoves = [];
-        const legalMoves = [];
+        const directions = [[1, 1], [-1, 1], [1, -1], [-1, -1], [1, 0], [-1, 0], [0, 1], [0, -1]]; 
+        const ourPiece = this
 
-        const locationRow = location.row;
-        const locationCol = location.col;
-
-        allMoves.push([locationRow + 1, locationCol + 1]);
-        allMoves.push([locationRow + 1, locationCol - 1]);
-        allMoves.push([locationRow - 1, locationCol + 1]);
-        allMoves.push([locationRow - 1, locationCol - 1]);
-        allMoves.push([locationRow, locationCol + 1]);
-        allMoves.push([locationRow, locationCol - 1]);
-        allMoves.push([locationRow + 1, locationCol]);
-        allMoves.push([locationRow - 1, locationCol]);
-
-        allMoves.forEach(move => {
-            if (move[0] > -1 && move[0] < 8 && move[1] > -1 && move[1] < 8) {
-                legalMoves.push(Square.at(move[0], move[1]));
-            }
-        });
-
-        return legalMoves;
+        return board.findMoves(directions, location, ourPiece, board, 1)    
+       
     }
 
 }
